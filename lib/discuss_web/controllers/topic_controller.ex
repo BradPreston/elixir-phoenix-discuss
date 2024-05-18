18840e9e67b98dfe3c75d2511c1397037201548f
bradpreston <bap5393@gmail.com>
2024-05-18 14:02:33 -0400

    Add create function in topic controller to create a new topic

--- lib/discuss_web/controllers/topic_controller.ex
+++ lib/discuss_web/controllers/topic_controller.ex
@@ -2,6 +2,7 @@ defmodule DiscussWeb.TopicController do
   use DiscussWeb, :controller
 
   alias Discuss.Topic
+  alias Discuss.Repo
 
   def new(conn, _params) do
     changeset = Topic.changeset(%Topic{}, %{})
@@ -13,5 +14,16 @@ defmodule DiscussWeb.TopicController do
   # of the params object. Now we have access to topic (and only topic)
   # in the create function
   def create(conn, %{"topic" => topic}) do
+    changeset = Topic.changeset(%Topic{}, topic)
+
+    case Repo.insert(changeset) do
+      {:ok, post} ->
+        nil
+
+      # tightly couples the form with the changeset. If any errors occur,
+      # the form will display the errors to the user
+      {:error, changeset} ->
+        render(conn, :new, changeset: changeset)
+    end
   end
 end
