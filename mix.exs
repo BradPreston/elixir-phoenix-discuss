fe3f87bb1052dcb88d3d4b572fdd891fa48a4551
bradpreston <bap5393@gmail.com>
2024-05-29 17:02:28 -0400

    Add ueberauth GitHub dependency

--- mix.exs
+++ mix.exs
@@ -58,7 +58,8 @@ defmodule Discuss.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
       {:bandit, "~> 1.2"},
-      {:ueberauth, "~> 0.10"}
+      {:ueberauth, "~> 0.10"},
+      {:ueberauth_github, "~> 0.8"}
     ]
   end
 
